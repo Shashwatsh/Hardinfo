9d5f75b5bb12493f92ecfd1ca6a9a82d742680ac
Igor Tkachenko <foozzione@gmail.com>
2016-11-14 17:36:22 -0800

    dev

---
+++ renderer.js
@@ -0,0 +1,29 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+(function(){
+    window.$ = window.jQuery = require('jquery');
+    const si = require('systeminformation')
+    const {BrowserWindow} = require('electron').remote
+    si.cpu()
+        .then(data => $('#cpu').text(data.brand))
+        .catch(error => console.error(error));
+    function init() {
+        // Minimize task
+        document.getElementById("min-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.minimize();
+        });
+
+        // Close app
+        document.getElementById("close-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.close();
+        });
+    }
+    document.onreadystatechange =  () => {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+})();
