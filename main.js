ffc1f8242366446cc4c98f8ed601301460366e23
Igor Tkachenko <foozzione@gmail.com>
2016-11-18 21:07:18 +0200

    dev...

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+// ipc electron 
+const ipcMain = electron.ipcMain
+// system info lib
+const si = require('systeminformation')
+// lib for paste to pastebin 
+const paste = require('pbin-guest')
 
 const path = require('path')
 const url = require('url')
@@ -10,19 +16,190 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-let infoWindow
 
-function createinfoWindow(render_data) {
-    infoWindow = new BrowserWindow({parent: top, modal: true, show: false})
-    child.loadURL({
-        pathname: path.join(__dirname, 'info.html'),
-        protocol: 'file:',
-        slashes: true,
-        render_data: render_data
+
+ipcMain.on('generate_info', function(event, args){
+  var output = '';
+
+  get_graphicsinfo()
+    .then(function(res){
+      console.log(res)
     })
-    child.once('ready-to-show', () => {
-      child.show()
+    .then(function(res){
+      console.log(res)
     })
+  
+
+  // .then(function(){
+  //   get_batteryinfo(function(err, res){
+  //     if(!err) {
+  //       list_info += '--- Battery info ---\n\r';
+  //       Object.keys(res).forEach(function(key) {
+  //         list_info += key+': '+res[key]+'\n\r';
+  //       });
+  //       return;
+  //     }
+  //   })
+  // }, function handleError(err){
+  //   console.error(err)
+  // })
+
+  // console.log(list_info)
+
+  // var graphics = new Promise(function(resolve, reject) {
+    
+  // })
+
+ 
+  // graphics.then(function(val){
+  //   console.log(val)
+  // })
+  // battery.then(function(val){
+  //   console.log(val)
+  // })
+
+
+  // get main info device
+  si.system(function(res, err){
+    hardinfo = {
+      manufacturer: res.manufacturer,
+      model: res.model,
+      serial: res.serial,
+      uuid: res.uuid
+    }
+  });
+  // get main info OS
+  si.osInfo(function(res, err){
+    if(err) {
+      event.sender.send('generate_info', {
+        err: err,
+        res: null
+      })
+      return;
+    }
+    platforminfo = {
+      platform: res.platform,
+      distro: res.distro,
+      release: res.release,
+      uuid: res.uuid,
+      codename: (process.platform === 'darwin' ? res.codename : ''),
+      kernel: res.kernel,
+      arch: res.arch,
+      hostname: res.hostname,
+      logofile: res.logofile
+    }
+  })
+  // get main info cpu
+  si.cpu(function(res, err){
+    if(err) {
+      event.sender.send('generate_info', {
+        err: err,
+        res: null
+      })
+      return;
+    }
+    cpuinfo = {
+      manufacturer: res.manufacturer,
+      brand: res.brand,
+      speed_core: res.speed,
+      cores_count: res.cores
+    }
+  })
+  // get main info memory
+  si.mem(function(res, err){
+    if(err) {
+      event.sender.send('generate_info', {
+        err: err,
+        res: null
+      })
+      return;
+    }
+    meminfo = {
+      total_memory: res.total,
+      swaptotal: res.swaptotal
+    }
+    
+    
+  })
+});
+
+function get_batteryinfo() {
+  const _i = new Promise(function(resolve, reject){
+    // get main info battery
+    si.battery(function(res, err){
+      if(err) {
+        reject(err)
+      }
+      batteryinfo = new Array();
+
+      batteryinfo['hasbattery'] = res.hasbattery;
+      batteryinfo['maxcapacity'] = res.maxcapacity;
+
+      
+      list_info = '--- Battery info ---\n\r';
+      Object.keys(batteryinfo).forEach(function(key) {
+        list_info += key+': '+batteryinfo[key]+'\n\r';
+      });
+      resolve(list_info)
+    })
+  }).then(function(resolve, reject){
+    si.graphics(function(res, err){
+      if(err) {
+        reject(reject)
+      }
+
+      graphicsinfo = new Array();
+
+      graphicsinfo['model'] = res.controllers[0].model;
+      graphicsinfo['vendor'] = res.controllers[0].vendor;
+      graphicsinfo['videoram'] = res.controllers[0].vram;
+      graphicsinfo['display_model'] = res.displays[0].model;
+      graphicsinfo['display_connection'] = res.displays[0].connection;
+      graphicsinfo['resolutionx'] = res.displays[0].resolutionx;
+      graphicsinfo['resolutiony'] = res.displays[0].resolutiony;
+      // graphicsinfo['depth'] = res.displays[0].depth;
+
+
+      list_info = '--- Graphics info ---\n\r';
+      Object.keys(graphicsinfo).forEach(function(key) {
+        list_info += key+': '+graphicsinfo[key]+'\n\r';
+      });
+      resolve(list_info)
+    })
+  })
+  return _i;
+}
+
+const resolve = console.log, 
+      reject  = console.log;
+
+function get_graphicsinfo() {
+  const _i = new Promise(function (resolve, reject) {
+    si.graphics(function(res, err){
+      if(err) {
+        reject(reject)
+      }
+
+      graphicsinfo = new Array();
+
+      graphicsinfo['model'] = res.controllers[0].model;
+      graphicsinfo['vendor'] = res.controllers[0].vendor;
+      graphicsinfo['videoram'] = res.controllers[0].vram;
+      graphicsinfo['display_model'] = res.displays[0].model;
+      graphicsinfo['display_connection'] = res.displays[0].connection;
+      graphicsinfo['resolutionx'] = res.displays[0].resolutionx;
+      graphicsinfo['resolutiony'] = res.displays[0].resolutiony;
+      // graphicsinfo['depth'] = res.displays[0].depth;
+
+
+      list_info = '--- Graphics info ---\n\r';
+      Object.keys(graphicsinfo).forEach(function(key) {
+        list_info += key+': '+graphicsinfo[key]+'\n\r';
+      });
+      resolve(list_info)
+    })
+  })
+  return _i;
 }
 
 function createWindow () {
--- renderer.js
+++ renderer.js
@@ -3,11 +3,11 @@
 // All of the Node.js APIs are available in this process.
 (function(){
     window.$ = window.jQuery = require('jquery');
-    const si = require('systeminformation')
-    const {BrowserWindow} = require('electron').remote
-    si.cpu()
-        .then(data => $('#cpu').text(data.brand))
-        .catch(error => console.error(error));
+
+    // const {BrowserWindow} = require('electron').remote
+    // si.cpu()
+    //     .then(data => $('#cpu').text(data.brand))
+    //     .catch(error => console.error(error));
     function init() {
         // Minimize task
         document.getElementById("min-btn").addEventListener("click", (e) => {
@@ -24,6 +24,13 @@
     document.onreadystatechange =  () => {
         if (document.readyState == "complete") {
             init();
+
+            ipcRenderer = require('electron').ipcRenderer
+
+            ipcRenderer.send('generate_info', '')
+            ipcRenderer.once('generate_info', function(event, arg){
+                console.log(arg)
+            })
         }
     };
 })();
